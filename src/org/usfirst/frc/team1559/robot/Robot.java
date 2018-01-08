c831ea2026c6e1df988b5f5b37b2c72ccc07bc2b
Ryan Luu <rl65536@gmail.com>
2018-01-08 16:40:49 -0500

    Added empty robot project

---
+++ src/org/usfirst/frc/team1559/robot/Robot.java
@@ -0,0 +1,58 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team1559.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+public class Robot extends IterativeRobot {
+
+	@Override
+	public void robotInit() {
+
+	}
+
+	@Override
+	public void autonomousInit() {
+
+	}
+
+	@Override
+	public void autonomousPeriodic() {
+
+	}
+	
+	@Override
+	public void teleopInit() {
+		
+	}
+
+	@Override
+	public void teleopPeriodic() {
+
+	}
+	
+	@Override
+	public void disabledInit() {
+		
+	}
+	
+	@Override
+	public void disabledPeriodic() {
+		
+	}
+
+	@Override
+	public void testInit() {
+		
+	}
+	
+	@Override
+	public void testPeriodic() {
+
+	}
+}
