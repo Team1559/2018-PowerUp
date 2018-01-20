e3e40449bd355f8a7c85dd402b35340ba5d829ac
Lucariax <lucariax@hotmail.com>
2018-01-20 09:50:53 -0500

    Added Climber and Expanded Wiring
    + Climber Class
    + Some placeholder Wiring values.

--- src/org/usfirst/frc/team1559/robot/Wiring.java
+++ src/org/usfirst/frc/team1559/robot/Wiring.java
@@ -1,12 +1,26 @@
 package org.usfirst.frc.team1559.robot;
 
-public class Wiring {
-
-	public static final int FR_SRX = 11;
-	public static final int RR_SRX = 13;
-	public static final int RL_SRX = 12;
-	public static final int FL_SRX = 10;
+public interface Wiring {
+	// Drive Train
+	final int FR_SRX = 11;
+	final int RR_SRX = 13;
+	final int RL_SRX = 12;
+	final int FL_SRX = 10;
 	
-	public static final int JOY_DRIVER = 0;
-	public static final int JOY_COPILOT = 1;
+	final int JOY_DRIVER = 0;
+	final int JOY_COPILOT = 1;
+	
+	// TODO: Find values. (Zeroes below are placeholders).
+	
+	// Gatherer
+	final int GATHERER_TALON = 0;
+	final int GATHERER_SOLENOID = 0;
+	// Lifter
+	final int LIFTER_TALON = 0;
+	// Climber
+	final int CLIMBER_SOLENOID = 0;
+	// Controller
+	final int BTN_CLIMB = 0;
+	final int BTN_LIFT = 0;
+	final int BTN_GRAB = 0;
 }
