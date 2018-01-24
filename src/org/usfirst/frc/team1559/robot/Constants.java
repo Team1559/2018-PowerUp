9801d498be045f46cff3acc07a653b4d8a015205
Jakku Man <32721112+Jakkuman@users.noreply.github.com>
2018-01-24 17:54:11 -0500

    Minor change, sorry
    #Cobblestone=Life
    #Dirt=Death
    #Soul Sand = Poison
    ☻

--- src/org/usfirst/frc/team1559/robot/Constants.java
+++ src/org/usfirst/frc/team1559/robot/Constants.java
@@ -10,7 +10,7 @@ public interface Constants {
 	public final double LIFT_HEIGHT = 0;
 	public final double LIFT_GEAR_DIAMETER = 0;
 	public final double LIFT_TIME = 0;
-	public final double GEAR_RATIO = 1 / 3; // This is a one to three ratio
+	public final double GEAR_RATIO = 1 / 3; // This is a one to three gear ratio
 	public final double LIFT_SPEED = ((LIFT_HEIGHT / Math.PI * LIFT_GEAR_DIAMETER * LIFT_TIME) * GEAR_RATIO); // This is lifter speed
 	
 }
