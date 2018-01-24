592332fed11c9dbbdd2f7ec69248b9e78eaee932
Jakku Man <32721112+Jakkuman@users.noreply.github.com>
2018-01-23 20:46:39 -0500

    And...I had to fix the public problem.
    Plz don't rage at me John. #cobblestone=life

--- src/org/usfirst/frc/team1559/robot/Constants.java
+++ src/org/usfirst/frc/team1559/robot/Constants.java
@@ -2,15 +2,15 @@ package org.usfirst.frc.team1559.robot;
 
 public interface Constants {
 	
-	final double P_DRIVER = 0; // These four are the four pit function values
-	final double I_DRIVER = 0;
-	final double D_DRIVER = 0;
-	final double F_DRIVER = 0;
-	final double CLIMB_SPEED = 0; // This is climber speed
-	final double LIFT_HEIGHT = 0;
-	final double LIFT_GEAR_DIAMETER = 0;
-	final double LIFT_TIME = 0;
-	final double GEAR_RATIO = 1 / 3; // This is a one to three ratio
-	final double LIFT_SPEED = ((LIFT_HEIGHT / Math.PI * LIFT_GEAR_DIAMETER * LIFT_TIME) * GEAR_RATIO); // This is lifter speed
+	public final double P_DRIVER = 0; // These four are the four pit function values
+	public final double I_DRIVER = 0;
+	public final double D_DRIVER = 0;
+	public final double F_DRIVER = 0;
+	public final double CLIMB_SPEED = 0; // This is climber speed
+	public final double LIFT_HEIGHT = 0;
+	public final double LIFT_GEAR_DIAMETER = 0;
+	public final double LIFT_TIME = 0;
+	public final double GEAR_RATIO = 1 / 3; // This is a one to three ratio
+	public final double LIFT_SPEED = ((LIFT_HEIGHT / Math.PI * LIFT_GEAR_DIAMETER * LIFT_TIME) * GEAR_RATIO); // This is lifter speed
 	
 }
