6c1c72b9b62061f181c184d66b1bb1fb7bdbb4d3
Lucariax <lucariax@hotmail.com>
2018-01-25 18:32:38 -0500

    Climber
    + Fixes

--- src/org/usfirst/frc/team1559/robot/Climber.java
+++ src/org/usfirst/frc/team1559/robot/Climber.java
@@ -1,60 +1,30 @@
 package org.usfirst.frc.team1559.robot;
 
-import edu.wpi.first.wpilibj.Solenoid;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 public class Climber {
-	/*
-	 * If you have any questions about the code, ask for Michael Nersinger or
-	 * Joshua Ingerowski from the Software Team.
-	 * 
-	 * The Climber subsystem uses a Solenoid to attach to the bar
-	 * in end game. Part of the subsystem is allowing other robots to
-	 * climb upon our robot, but as this is not a function that needs to be
-	 * programmed, it is not referenced in this code. This code is used for the
-	 * 2018 Game First Power Up. The type of climber used is a telescope which
-	 * extends outward in smaller layers each time;
-	 */
-	private Solenoid climber;
-	/**
-	 * 
-	 * @param port
-	 * The number on the electrical board used for the Climber Solenoid.
-	 **/
+	private WPI_TalonSRX climber;
+
 	public Climber(int port) {
-		/**
-		 * The Climber Constructor instantiates the field 'climber' at the
-		 * specified port.
-		 **/
-		climber = new Solenoid(port);
+		climber = new WPI_TalonSRX(port);
 	}
 
-	public void extendClimb() {
-		/**
-		 * The startClimb method sets the field 'climber' to true.
-		 * (This allows the climber to extend up and prepare to
-		 * climb)
-		 **/
-		climber.set(true);
+	public void startClimb() {
+		climber.set(Constants.CLIMB_SPEED);
 	}
 
-	public void retractClimb() {
-		/**
-		 * The unClimb method sets the field 'climber' to false.
-		 * (This causes the climber to retract and complete the
-		 * climb)
-		 **/
-		climber.set(false);
+	public void reverseClimb() {
+		climber.set(-Constants.CLIMB_SPEED);
+	}
+	
+	public void stopClimb() {
+		climber.set(0);
 	}
 
 	private static Climber instance;
-	/**
-	 * 
-	 * @return Returns the instance of the climber class.
-	 */
+
 	public static Climber getInstance() {
-		/* 
-		 * Returns the instance of the climber class.
-		 */
+
 		if (instance == null) {
 			instance = new Climber(Wiring.CLIMBER_SOLENOID);
 		}
