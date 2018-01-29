d1737998cd26594358f529480503b65af77a0ede
Jakku Man <32721112+Jakkuman@users.noreply.github.com>
2018-01-29 18:35:43 -0500

    Made this slightly easier to read
    Plz don't get mad at me.
    #Cobblestone = Life
    #Dirt = Death
    #Soul sand = The Void

--- src/org/usfirst/frc/team1559/robot/Constants.java
+++ src/org/usfirst/frc/team1559/robot/Constants.java
@@ -9,13 +9,17 @@ public interface Constants {
 	public final double I_DRIVER = 0;
 	public final double D_DRIVER = 0;
 	public final double F_DRIVER = 0;
+	
 	public final double CLIMB_SPEED = 0; // This is climber speed
+	
 	public final double LIFT_HEIGHT = 0;
 	public final double LIFT_GEAR_DIAMETER = 0;
 	public final double LIFT_TIME = 0;
+	
 	public final double GEAR_RATIO = 1 / 3; // This is a one to three gear ratio
+	
 	public final double LIFT_SPEED = ((LIFT_HEIGHT / Math.PI * LIFT_GEAR_DIAMETER * LIFT_TIME) * GEAR_RATIO); // This is
-																												// lifter
+																										// lifter
 																												// speed
 
 }
