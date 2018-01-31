62bddbb2f2b56eddee5b1e19f17a65ef890a3330
Matt <hartlem@victorschools.org>
2018-01-30 19:43:26 -0500

    gatherer implementation
    :3
    :b:

---
+++ src/org/usfirst/frc/team1559/robot/Grabber.java
@@ -0,0 +1,30 @@
+package org.usfirst.frc.team1559.robot;
+
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Spark;
+
+public class Grabber {
+
+	private Solenoid sally;
+	private Spark sparky1; 
+	private Spark sparky2;
+	private Spark sparkus1;
+	private Spark sparkus2; 
+	
+	//Talon sr or Sparks
+	public Grabber(int port1, int port2, int port3, int port4, int port5)
+	{
+		
+	}
+	public void horizontalMove() //Opens and closes pincers while turning belt
+	{
+		
+  	}
+	public void verticalMove() //Lift and lower grabber
+	{
+		
+	}
+}
+	
+	
+
