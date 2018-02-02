3c3638a909137bd07891f59c9a8bf9e4d37bbc1c
Lucariax <lucariax@hotmail.com>
2018-02-01 19:13:06 -0500

    Updates to Climber
    + Updtaed methods, TalonSR, Spark, DigitalInput
    + More constants, wiring.
    - Useless Junk

--- src/org/usfirst/frc/team1559/robot/Climber.java
+++ src/org/usfirst/frc/team1559/robot/Climber.java
@@ -1,23 +1,36 @@
 package org.usfirst.frc.team1559.robot;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.Talon;
 
 public class Climber {
-	private Spark climber;
+	private Spark climberSpark;
+	private Talon climberTalon;
+	private DigitalInput climberSparkLimit;
 
-	public Climber(int port) {
-		climber = new Spark(port);
+	public Climber() {
+		climberSpark = new Spark(Wiring.CLIMBER_SPARK); // Up and down telescope
+		climberTalon = new Talon(Wiring.CLIMBER_TALON); // Winch function - the wheel that flips around
+		climberSparkLimit = new DigitalInput(Wiring.CLIMBER_LIMIT_ID);
 	}
 
-	public void startClimb() {
-		climber.set(Constants.CLIMB_SPEED);
+	public void extendTelescope() {
+		if(!climberSparkLimit.get()) {
+			climberSpark.set(Constants.CLIMB_TELESCOPE_SPEED);
+		} else {
+			climberSpark.set(0);
+		}
 	}
 
-	public void reverseClimb() {
-		climber.set(-Constants.CLIMB_SPEED);
+	public void retractTelescope() {
+		climberSpark.set(-Constants.CLIMB_TELESCOPE_SPEED);
 	}
 	
-	public void stopClimb() {
-		climber.set(0);
+	public void rotateWinch() {
+		climberTalon.set(Constants.CLIMB_TALON_SPEED);
+	}
+	public void stopWinch() {
+		climberTalon.set(0);
 	}
 }
