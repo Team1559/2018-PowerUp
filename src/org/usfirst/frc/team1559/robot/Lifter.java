2e0e13584cbe3a1ca31cf13ee862229b7bae3c75
Jakku Man <32721112+Jakkuman@users.noreply.github.com>
2018-02-01 19:46:12 -0500

    Lifter update
    Plz don't roast us too badly.
    Max + Jack
    Really, plz don't.
    #Cobblestone = Life

--- src/org/usfirst/frc/team1559/robot/Lifter.java
+++ src/org/usfirst/frc/team1559/robot/Lifter.java
@@ -1,5 +1,23 @@
 package org.usfirst.frc.team1559.robot;
 
-public class Lifter {
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj.interfaces.Potentiometer;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Joystick;
 
+public class Lifter {
+	Potentiometer lift;
+	AnalogInput ai;
+	DigitalInput limit;
+	Joystick joy;
+
+	public Lifter(int a, int button) {
+		ai = new AnalogInput(a);
+		if (joy.getRawButton(button))
+			lift = new AnalogPotentiometer(ai, 360, 30);
+		else
+			lift = new AnalogPotentiometer(ai, 0, 30);
+
+	}
 }
