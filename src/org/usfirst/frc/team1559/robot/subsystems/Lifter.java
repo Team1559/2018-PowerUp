fe5f4d647ec2ac32a4073a8062fe8a58c4efc0e5
Jakku Man <32721112+Jakkuman@users.noreply.github.com>
2018-02-14 18:24:24 -0500

    Revert "Merge branch 'master' of https://github.com/Team1559/2018-PowerUp"
    This reverts commit 9e74edf028a102a2cee8e6be4926ecbc17094fb6, reversing
    changes made to 36a7580943f43ff8d65dd8933632b224356bbb8a.

--- src/org/usfirst/frc/team1559/robot/subsystems/Lifter.java
+++ src/org/usfirst/frc/team1559/robot/subsystems/Lifter.java
@@ -4,6 +4,8 @@ import org.usfirst.frc.team1559.robot.Constants;
 import org.usfirst.frc.team1559.robot.Wiring;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.AnalogPotentiometer;
@@ -11,39 +13,53 @@ import edu.wpi.first.wpilibj.interfaces.Potentiometer;
 
 public class Lifter {
 
-	private Potentiometer potentiometer;
-	private WPI_TalonSRX motor;
+	private WPI_TalonSRX lifterMotor;
+	private final int TIMEOUT = 0;
+	private double kP = 0.1;
+	private double kI = 0;
+	private double kD = 0;
+	private double kF = 0;
 
 	public Lifter() {
-		potentiometer = new AnalogPotentiometer(Wiring.LFT_POT);
-		motor = new WPI_TalonSRX(Wiring.LFT_TALON);
+		lifterMotor = new WPI_TalonSRX(Wiring.LIFT_TALON);
+		lifterMotor.set(ControlMode.Position, 0);
+		lifterMotor.configSelectedFeedbackSensor(FeedbackDevice.Analog, 0, TIMEOUT);
+		
+		lifterMotor.configNominalOutputForward(0, TIMEOUT);
+		lifterMotor.configNominalOutputReverse(0, TIMEOUT);
+		lifterMotor.configPeakOutputForward(+1, TIMEOUT);
+		lifterMotor.configPeakOutputReverse(-1, TIMEOUT);
+
+		lifterMotor.config_kP(0, kP, TIMEOUT);
+		lifterMotor.config_kI(0, kI, TIMEOUT);
+		lifterMotor.config_kD(0, kD, TIMEOUT);
+		lifterMotor.config_kF(0, kF, TIMEOUT);
+		
+		lifterMotor.setSensorPhase(true);
+		lifterMotor.setNeutralMode(NeutralMode.Brake);
 	}
 
 	public double getPot() {
-		return motor.getSensorCollection().getAnalogIn();
+		return lifterMotor.getSensorCollection().getAnalogIn();
 	}
 	
 	public void toSwitch() {
-		motor.set(ControlMode.Position, Constants.SWITCH_TOP_LIMIT);
+		lifterMotor.set(ControlMode.Position, Constants.SWITCH_TOP_LIMIT);
 	}
 	public void toBottomScale() {
-		motor.set(ControlMode.Position, Constants.SCALE_BOTTOM_LIMIT);
+		lifterMotor.set(ControlMode.Position, Constants.SCALE_BOTTOM_LIMIT);
 
 	}
 	public void toNeutralScale() {
-		motor.set(ControlMode.Position, Constants.SCALE_NEUTRAL_LIMIT);
+		lifterMotor.set(ControlMode.Position, Constants.SCALE_NEUTRAL_LIMIT);
 
 	}
 	public void toTopScale() {
-		motor.set(ControlMode.Position, Constants.SCALE_TOP_LIMIT);
+		lifterMotor.set(ControlMode.Position, Constants.SCALE_TOP_LIMIT);
+
 	}
 
-	public void goDown() {
-		if (potentiometer.get() > Constants.LIFT_BOTTOM_LIMIT && potentiometer.get() >= Wiring.BTN_LIFT_GROUND) {
-			motor.set(-Constants.LIFT_SPEED);
-		} else {
-			motor.set(0);
-		}
-
+	public void toHome() {
+		lifterMotor.set(ControlMode.Position, Constants.LIFT_BOTTOM_LIMIT);
 	}
 }
