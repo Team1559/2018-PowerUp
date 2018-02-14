c9fac88e0a85eaf7817886f939992540bf063ff8
Ryan Luu <rl65536@gmail.com>
2018-02-14 18:20:25 -0500

    Revert "Revert "Mike I hate your laptop""
    This reverts commit 32c16ce48f038c6d7e0ab8ed72bf380a923bf448.

--- src/org/usfirst/frc/team1559/robot/Constants.java
+++ src/org/usfirst/frc/team1559/robot/Constants.java
@@ -6,7 +6,7 @@ public interface Constants {
 	public final double DT_SPROCKET_RATIO = 32.0 / 22.0;
 	public final double WHEEL_FUDGE = 0.723;
 	
-	public final double P_DRIVER = 0; // These four are the four pid function values
+	public final double P_DRIVER = 0; // These four are the four pit function values
 	public final double I_DRIVER = 0;
 	public final double D_DRIVER = 0;
 	public final double F_DRIVER = 0;
@@ -20,4 +20,11 @@ public interface Constants {
 	public final int SCALE_NEUTRAL_LIMIT = 0;
 	public final int SCALE_BOTTOM_LIMIT = 0;
 	public final int SWITCH_TOP_LIMIT = 0;
+	
+	public final double LIFT_HEIGHT = 0;
+	public final double LIFT_GEAR_DIAMETER = 0;
+	public final double LIFT_TIME = 0;
+	public final double GEAR_RATIO = 1 / 3; // This is a one to three gear ratio
+	public final double LIFT_SPEED = ((LIFT_HEIGHT / Math.PI * LIFT_GEAR_DIAMETER * LIFT_TIME) * GEAR_RATIO); // This is lifter speed
+
 }
